0dbb451fb5b2bebf9b7fc1b4a9ed4b2950e13926
Josh Hall <jch2617@gmail.com>
2023-10-21 16:11:31 -0400

    feat: add form-submission page

---
+++ src/app/form-submissions/page.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import styles from "../page.module.css";
+
+export default function FormSubmissions() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <Typography>foo</Typography>
+      </div>
+    </main>
+  );
+}
