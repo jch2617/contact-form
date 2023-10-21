503cf29444dc7b1b69991730b10e3788604f565e
Josh Hall <jch2617@gmail.com>
2023-10-20 22:47:09 -0400

    feat: add email validation

---
+++ public/components/Form.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+const defaultBlue = "#0087AA";
+const hoverBlue = "#1997B7";
+
+const textFieldStyles = {
+  background: "#ffffff",
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: hoverBlue,
+    },
+    "&:hover fieldset": {
+      borderColor: hoverBlue,
+    },
+    "& .Mui-focused": {
+      border: defaultBlue,
+    },
+  },
+  "& .MuiFormLabel-root": {
+    color: defaultBlue,
+    "& .Mui-error": {
+      color: "red",
+    },
+  },
+};
+
+const PersonEdit: React.FC = () => {
+  const [formData, setFormData] = useState({});
+  const [isValidEmail, setIsValidEmail] = useState<boolean>(true);
+
+  const emailPattern =
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  const handleChangeEmail = (e: any) => {
+    const reg = new RegExp(emailPattern);
+    setIsValidEmail(reg.test(e.target.value));
+    setFormData({ ...formData, email: e.target.value });
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "25rem",
+        gap: "1rem",
+      }}
+    >
+      <Typography variant="h5">Contact Us Form</Typography>
+      <TextField
+        label="First Name"
+        sx={textFieldStyles}
+        onChange={(e) => {
+          setFormData({ ...formData, firstName: e.target.value });
+        }}
+      >
+        First Name
+      </TextField>
+      <TextField
+        label="Last Name"
+        sx={textFieldStyles}
+        onChange={(e) => {
+          setFormData({ ...formData, lastName: e.target.value });
+        }}
+      >
+        Last Name
+      </TextField>
+      <TextField
+        label="Email"
+        sx={textFieldStyles}
+        error={!isValidEmail}
+        onChange={(e) => {
+          handleChangeEmail(e);
+        }}
+      >
+        Email
+      </TextField>
+      <TextField
+        label="Message"
+        multiline
+        minRows={4}
+        sx={textFieldStyles}
+        onChange={(e) => {
+          setFormData({ ...formData, message: e.target.value });
+        }}
+      />
+      <Button
+        disabled={!isValidEmail}
+        variant="contained"
+        sx={{
+          backgroundColor: defaultBlue,
+          ":hover": {
+            backgroundColor: hoverBlue,
+          },
+        }}
+        onClick={() => {
+          console.log(formData);
+        }}
+      >
+        Submit
+      </Button>
+    </Box>
+  );
+};
+
+export default PersonEdit;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,12 @@
-import { Box, Button, TextField, Typography } from '@mui/material'
-import styles from './page.module.css'
-
-/* TODO
-- check mobile
-- clean up css
-- validations
-- form state
-*/
-
-const defaultBlue = "#0087AA";
-const hoverBlue = "#1997B7";
-
-const textFieldStyles = {
-  background: '#ffffff',
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: hoverBlue,
-    },
-    '&:hover fieldset': {
-      borderColor: hoverBlue,
-    },
-    "& .Mui-focused": {
-      border: defaultBlue,
-    }
-  },
-  '& .MuiFormLabel-root': {
-    color: defaultBlue,
-  },
-}
+import Form from "../../public/components/Form";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <Box className={styles.form}>
-          <Typography variant="h5">Contact Us Form</Typography>
-          <TextField label="First Name" sx={textFieldStyles}>First Name</TextField>
-          <TextField label="Last Name" sx={textFieldStyles}>Last Name</TextField>
-          <TextField label="Email" sx={textFieldStyles}>Email</TextField>
-          <TextField
-            label="Message"
-            multiline
-            minRows={4}
-            sx={textFieldStyles}
-          />    
-          <Button 
-            variant="contained" 
-            sx={{
-              backgroundColor: defaultBlue,
-              ":hover": {
-                backgroundColor: hoverBlue
-              }
-            }}
-          >
-            Submit
-          </Button>  
-        </Box>
+        <Form />
       </div>
     </main>
-  )
+  );
 }
