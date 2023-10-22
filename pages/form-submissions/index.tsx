3836cd2beb9c96c015522108d44046c96f3d4b14
Josh Hall <jch2617@gmail.com>
2023-10-21 22:27:22 -0400

    feat: stub out form submission page

--- pages/form-submissions/index.tsx
+++ pages/form-submissions/index.tsx
@@ -1,5 +1,32 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { FormSubmissionResponse } from "../../types/FormSubmissionRequest";
 
 export default function FormSubmissions() {
-  return <Typography>Form Submissions</Typography>;
+  const [formSubmissions, setFormSubmissions] = useState([]);
+
+  const getSubmissions = async () => {
+    const response = await fetch("../api/formSubmissions");
+    const data = await response.json();
+    console.log(data);
+    setFormSubmissions(data);
+  };
+
+  useEffect(() => {
+    getSubmissions();
+  }, []);
+
+  return (
+    <Box>
+      <Typography>Form Submissions</Typography>
+      {formSubmissions.map((s: FormSubmissionResponse) => {
+        // TODO make a table
+        return (
+          <div key={s.id}>
+            {s.email} {s.message}
+          </div>
+        );
+      })}
+    </Box>
+  );
 }
