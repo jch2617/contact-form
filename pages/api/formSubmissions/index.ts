3208ba7a4160af856d2091b5d09891d09963a49d
Josh Hall <jch2617@gmail.com>
2023-10-21 22:59:44 -0400

    refactor: remove form submissions page and put results in home page

--- pages/api/formSubmissions/index.ts
+++ pages/api/formSubmissions/index.ts
@@ -1,19 +1,18 @@
 import { formSubmissions } from "../../../data/formSubmissions";
 import { FormSubmissionResponse } from "../../../types/FormSubmissionRequest";
 import { FormSubmissionRequest } from "../../../types/FormSubmissionResponse";
- 
+
 export default function handler(req: any, res: any) {
   if (req.method === "GET") {
     res.status(200).json(formSubmissions);
   } else if (req.method === "POST") {
-    console.log("req", req.body);
     const submission: FormSubmissionRequest = req.body.formData;
     const newSubmission: FormSubmissionResponse = {
       id: Date.now(),
       email: submission.email,
       firstName: submission.firstName,
       lastName: submission.lastName,
-      message: submission.lastName,
+      message: submission.message,
     };
     formSubmissions.push(newSubmission);
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
 import type { ReactElement } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../public/components/layout";
 import styles from "../src/app/page.module.css";
 import type { NextPageWithLayout } from "./_app";
+import { FormSubmissionResponse } from "../types/FormSubmissionRequest";
 
 const defaultBlue = "#0087AA";
 const hoverBlue = "#1997B7";
@@ -32,6 +33,7 @@ const textFieldStyles = {
 const ContactForm: React.FC = () => {
   const [formData, setFormData] = useState({});
   const [isValidEmail, setIsValidEmail] = useState<boolean>(true);
+  const [formSubmissions, setFormSubmissions] = useState([]);
 
   const emailPattern =
     /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -41,6 +43,17 @@ const ContactForm: React.FC = () => {
     setIsValidEmail(reg.test(e.target.value));
     setFormData({ ...formData, email: e.target.value });
   };
+
+  const getSubmissions = async () => {
+    const response = await fetch("../api/formSubmissions");
+    const data = await response.json();
+    console.log(data);
+    setFormSubmissions(data);
+  };
+
+  useEffect(() => {
+    getSubmissions();
+  }, []);
 
   const submitForm = async () => {
     const response = await fetch("./api/formSubmissions", {
@@ -55,76 +68,97 @@ const ContactForm: React.FC = () => {
   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        width: "25rem",
-        gap: "1rem",
-      }}
-    >
-      <Typography variant="h5">Contact Us Form</Typography>
-      <TextField
-        label="First Name"
-        sx={textFieldStyles}
-        onChange={(e) => {
-          setFormData({ ...formData, firstName: e.target.value });
-        }}
-      >
-        First Name
-      </TextField>
-      <TextField
-        label="Last Name"
-        sx={textFieldStyles}
-        onChange={(e) => {
-          setFormData({ ...formData, lastName: e.target.value });
-        }}
-      >
-        Last Name
-      </TextField>
-      <TextField
-        label="Email"
-        sx={textFieldStyles}
-        error={!isValidEmail}
-        onChange={(e) => {
-          handleChangeEmail(e);
-        }}
-      >
-        Email
-      </TextField>
-      <TextField
-        label="Message"
-        multiline
-        minRows={4}
-        sx={textFieldStyles}
-        onChange={(e) => {
-          setFormData({ ...formData, message: e.target.value });
-        }}
-      />
-      <Button
-        disabled={!isValidEmail}
-        variant="contained"
+    <>
+      <Box
         sx={{
-          backgroundColor: defaultBlue,
-          ":hover": {
-            backgroundColor: hoverBlue,
-          },
-        }}
-        onClick={() => {
-          return submitForm();
+          display: "flex",
+          flexDirection: "column",
+          width: "25rem",
+          gap: "1rem",
+          justifyContent: "center",
+          alignItems: "inherit",
+          maxWidth: "var(--max-width)",
+          zIndex: 2,
+          fontFamily: "var(--font-mono)",
+          position: "relative",
+          margin: 0,
+          padding: "1rem",
+          backgroundColor: "rgba(var(--callout-rgb), 0.5)",
+          border: "1px solid rgba(var(--callout-border-rgb), 0.3)",
+          borderRadius: "var(--border-radius)",
         }}
       >
-        Submit
-      </Button>
-    </Box>
+        <Typography variant="h5">Contact Us Form</Typography>
+        <TextField
+          label="First Name"
+          sx={textFieldStyles}
+          onChange={(e) => {
+            setFormData({ ...formData, firstName: e.target.value });
+          }}
+        >
+          First Name
+        </TextField>
+        <TextField
+          label="Last Name"
+          sx={textFieldStyles}
+          onChange={(e) => {
+            setFormData({ ...formData, lastName: e.target.value });
+          }}
+        >
+          Last Name
+        </TextField>
+        <TextField
+          label="Email"
+          sx={textFieldStyles}
+          error={!isValidEmail}
+          onChange={(e) => {
+            handleChangeEmail(e);
+          }}
+        >
+          Email
+        </TextField>
+        <TextField
+          label="Message"
+          multiline
+          minRows={4}
+          sx={textFieldStyles}
+          onChange={(e) => {
+            setFormData({ ...formData, message: e.target.value });
+          }}
+        />
+        <Button
+          disabled={!isValidEmail}
+          variant="contained"
+          sx={{
+            backgroundColor: defaultBlue,
+            ":hover": {
+              backgroundColor: hoverBlue,
+            },
+          }}
+          onClick={() => submitForm()}
+        >
+          Submit
+        </Button>
+      </Box>
+      <Box>
+        <Typography>Form Submissions</Typography>
+        {formSubmissions.map((s: FormSubmissionResponse) => {
+          // TODO make a table
+          return (
+            <div key={s.id}>
+              {s.email} {s.message}
+            </div>
+          );
+        })}
+      </Box>
+    </>
   );
 };
 
 const Page: NextPageWithLayout = () => {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        {/* <Form /> */}
+      <div>
         <ContactForm />
       </div>
     </main>
